15143b9868da7de71e76176d4b410d6ab98f481a
michal.gajewski <michalpiotr.gajewski@gmail.com>
2022-10-21 10:07:33 +0200

    init

---
+++ src/main/java/pl/gajewski/sygnity/SygnityApplication.java
@@ -0,0 +1,13 @@
+package pl.gajewski.sygnity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SygnityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SygnityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gajewski/sygnity/SygnityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gajewski.sygnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SygnityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
